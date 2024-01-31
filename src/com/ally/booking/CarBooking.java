a9d0f92037aed974b6fbebaf8f39b9946d06b506
leandre <tuyambazeleandre@gmail.com>
2024-01-31 18:14:29 +0200

    Implement Booking plain class

---
+++ src/com/ally/booking/CarBooking.java
@@ -0,0 +1,92 @@
+package com.ally.booking;
+
+import com.ally.car.Car;
+import com.ally.user.User;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+public class CarBooking {
+    private UUID bookingId;
+    private User user;
+    private Car car;
+    private LocalDateTime bookingTime;
+    private boolean isCancelled;
+
+    public CarBooking(UUID bookingId, User user, Car car, LocalDateTime bookingTime, boolean isCancelled) {
+        this.bookingId = bookingId;
+        this.user = user;
+        this.car = car;
+        this.bookingTime = bookingTime;
+        this.isCancelled = isCancelled;
+    }
+
+    private CarBooking(UUID bookingId, User user, Car car, LocalDateTime bookingTime) {
+        this(bookingId, user, car, bookingTime, false);
+    }
+
+    public UUID getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(UUID bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public LocalDateTime getBookingTime() {
+        return bookingTime;
+    }
+
+    public void setBookingTime(LocalDateTime bookingTime) {
+        this.bookingTime = bookingTime;
+    }
+
+    public boolean isCancelled() {
+        return isCancelled;
+    }
+
+    public void setCancelled(boolean cancelled) {
+        isCancelled = cancelled;
+    }
+
+    @Override
+    public String toString() {
+        return "CarBooking{" +
+                "bookingId=" + bookingId +
+                ", user=" + user +
+                ", car=" + car +
+                ", bookingTime=" + bookingTime +
+                ", isCancelled=" + isCancelled +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CarBooking that = (CarBooking) o;
+        return isCancelled == that.isCancelled && Objects.equals(bookingId, that.bookingId) && Objects.equals(user, that.user) && Objects.equals(car, that.car) && Objects.equals(bookingTime, that.bookingTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookingId, user, car, bookingTime, isCancelled);
+    }
+}
