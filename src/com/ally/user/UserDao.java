38deab12efa424bc791cb48dcffe86ad5510eb20
leandre <tuyambazeleandre@gmail.com>
2024-02-03 03:46:35 +0200

    Add user interface for getUser and use file too

--- src/com/ally/user/UserDao.java
+++ src/com/ally/user/UserDao.java
@@ -1,19 +1,5 @@
 package com.ally.user;
 
-import java.util.UUID;
-
-public class UserDao {
-    private static final User[] users;
-
-    static {
-        users = new User[]{
-                new User(UUID.fromString("8ca51d2b-aaaf-4bf2-834a-e02964e10fc3"), "Leandre"),
-                new User(UUID.fromString("b10d126a-3608-4980-9f9c-aa179f5cebc3"), "John"),
-        };
-    }
-
-    public User[] getUsers(){
-        return users;
-    }
-
+public interface UserDao {
+    User[] getUsers();
 }
